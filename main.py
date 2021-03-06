dc34a1f117340686c472f2d962f926f80e48051c
Ardalan Ghoorchian <ardygH@berkeley.edu>
2021-03-05 17:44:22 -0800

    colors

--- main.py
+++ main.py
@@ -5,3 +5,16 @@ WIDTH = 800
 WIN = pygame.display.set_mode((WIDTH, WIDTH))
 pygame.display.set_caption("AI SEARCH STUDY")
 pygame.init()
+
+
+
+# ADDING COLORS
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+GREEN = (0, 255, 0)
+RED = (255, 0, 0)
+GREY = (128, 128, 128)
+
+
+
